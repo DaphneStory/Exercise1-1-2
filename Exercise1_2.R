ff4b61702a245ddc5f6ccfdc3e6ecbf12df4dd6c
DaphneStory <dstory@email.sc.edu>
2025-01-21 10:51:08 -0500

    class script 2025-01-21

--- Exercise1_2.R
+++ Exercise1_2.R
@@ -31,3 +31,42 @@ avg_depth_km*world_oceans$area_km2
 Travel = data.frame(oceans = c("Atlantic", "Pacific", "Indian", "Artic", "Southern"),
                     traveled = c(TRUE, TRUE, FALSE, FALSE, FALSE))
 Travel
+
+
+#2025-01-21 Script
+
+round(5.88998798435, digits = 2) # number of digits after period
+
+mean(world_oceans$avg_depth_m)
+
+round(mean(world_oceans$avg_depth_m))
+
+
+install.packages("ggplot2", dependencies = TRUE)
+library("ggplot2")
+summary(world_oceans)
+summary(oceans)
+
+
+random_data=c(1,"Atlantic", 3.7, TRUE)
+random_data #number turns into character
+random_data[1] + random_data[3] #throws error
+#coercion
+as.numeric(random_data[1]) + as.numeric(random_data[3])
+
+
+##Special Characters
+
+data = c(3,5,7.9, NA, 8)
+data
+mean(data, na.rm = TRUE) #rm is remove
+is.na(data) #is element an na
+sum(is.na(data)) #false is called 0 and true 1
+1/0
+Inf/Inf #NaN - not a number
+Inf +2
+0/0
+
+dat2 = c(1,2, NA, 3, NULL, 4)
+dat2
+length(dat2)
